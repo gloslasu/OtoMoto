8faf0bfcae581ba98f21a33b7ae591c716c0eb1a
slech <regmewww@gmail.com>
2023-07-04 08:52:52 +0200

    add PropetiesLoader class

---
+++ src/test/java/utils/propeties/PropetiesLoader.java
@@ -0,0 +1,38 @@
+package utils.propeties;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropetiesLoader {
+
+    public Properties getPropeties(String propetiesFileName) {
+
+        InputStream inputStream = null;
+        Properties properties = new Properties();
+        try {
+            inputStream = getClass().getClassLoader().getResourceAsStream(propetiesFileName);
+            if (inputStream != null) {
+                properties.load(inputStream);
+            } else {
+                throw new FileNotFoundException();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        } finally {
+            closeInputStream(inputStream);
+        }
+        return properties;
+    }
+
+
+    private void closeInputStream(InputStream inputStream) {
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
